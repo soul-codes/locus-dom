a7642d5be6a6bba96dde00f6a9b9a1d1f4160169
soul-codes <reach.soul.codes@gmail.com>
2018-06-17 19:09:28 +0200

    tsdocs, combine dev tasks

--- src/Locus.ts
+++ src/Locus.ts
@@ -6,41 +6,74 @@ interface ILocusState {
   left: number;
 }
 
+/**
+ * Synchronizes the position state readout of a DOM element.
+ */
 export default class Locus {
   private _state: ILocusState = { top: 0, left: 0 };
 
   constructor(
     readonly settings: {
+      /**
+       * Specifies the target DOM element.
+       */
       target: HTMLElement | (() => HTMLElement);
-      listener: (locus: Locus) => void;
+
+      /**
+       * Specifies the listener function that will be invoked
+       * when the layout observers report a change resulting in the
+       * observed element changing position.
+       */
+      listener: (locus: Locus) => any;
     }
   ) {}
 
+  /**
+   * Reflects back the configured Locus instance's target DOM element.
+   */
   get target(): HTMLElement {
     const { target } = this.settings;
     return typeof target === "function" ? target() : target;
   }
 
+  /**
+   * Returns the Locus instance's position state. Has no meaning if
+   * the Locus instance is not active.
+   */
   get state(): ILocusState {
     return { ...this._state };
   }
 
+  /**
+   * Returns true if the Locus instance is currently updating changes.
+   */
   get isActive() {
     return loci.has(this);
   }
 
+  /**
+   * Start observing positional changes. This always calls the listener
+   * once with the freshly calculated current position.
+   */
   start() {
     if (!this.isActive) {
       loci.add(this);
-      this.update();
+      this.update(true);
     }
   }
 
+  /**
+   * Stop observing positional changes.
+   */
   stop() {
     loci.delete(this);
   }
 
-  update() {
+  /**
+   * Force-updates the position state. May result in calling
+   * of the listener if the new position differs from the old one.
+   */
+  update(forceCallListener?: boolean) {
     const {
       target,
       settings: { listener }
@@ -48,7 +81,8 @@ export default class Locus {
     const position = getPosition(target);
     if (
       position.left !== this._state.left ||
-      position.top !== this._state.top
+      position.top !== this._state.top ||
+      forceCallListener
     ) {
       this._state.left = position.left;
       this._state.top = position.top;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ export { toggleLogging } from "./reporters";
 export { default as registerChangeReporter } from "./update";
 export { default as Locus } from "./Locus";
 export { getCurrentLoci } from "./store";
+export { default as getPosition } from "./getPosition";
 
+/**
+ * Install the built-in change reporters.
+ */
 export function install() {
   registerBuiltInChangeReporters();
 }
